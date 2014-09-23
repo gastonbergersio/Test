962f7587820cc05f13c35c4f5d9b7bbe83eefab1
gastonberger <julienvilvandre@yahoo.fr>
2014-09-23 17:43:06 +0200

    2 classes + constructeur

---
+++ src/test/Client.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author JV
+ */
+public class Client {
+   
+    private String nom;
+    public Client(String n){
+        this.nom=n;
+    }
+    
+    
+}
