c0b665eeb6e0699b6207fe00a77ea1cafbf89922
gastonberger <julienvilvandre@yahoo.fr>
2014-09-23 18:04:53 +0200

    Setteur OK

--- src/test/Test.java
+++ src/test/Test.java
@@ -17,6 +17,7 @@ public class Test {
     public static void main(String[] args) {
         // TODO code application logic here
         Client c = new Client("toto");
+        c.setNom("Jean-mich");
         System.out.println(c.getNom());
     }
     
